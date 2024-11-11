9bd8f7415aa27bd7d2262250b3494bcf563f37f2
Himanshu <himanshu.44909@gmail.com>
2024-11-11 22:57:49 +0530

    Database Changes

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = (url) => {
     console.log("Connect DB");
-    // return mongoose.connect(url, {
-    //     dbName: 'cure_near_db'
-    // });
-    return mongoose.connect(url);
+    return mongoose.connect(url, {
+        dbName: 'cure_near_db'
+    });
+    // return mongoose.connect(url);
 }
 
 module.exports = connectDB;
